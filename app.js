fac1d5307e7b22cc7c32692f5672aba4da296d21
amokdad <30440605+amokdad@users.noreply.github.com>
2017-10-28 12:18:31 +0300

    Commit All

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
-
+ 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
