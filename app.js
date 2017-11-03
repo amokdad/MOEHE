4c94dfc0e60bc2137bf98d9763c6a89f333f838a
amokdad <30440605+amokdad@users.noreply.github.com>
2017-11-03 20:12:42 +0300

    dsas

--- app.js
+++ app.js
@@ -117,6 +117,19 @@ bot.dialog('/', intents);
 bot.dialog("askQuestions",[
     function(session){
         //createRecord("name","role","service","mobile","value");
+
+        var card = builder.AudioCard(session)
+        .title('I am your father')
+        .subtitle('Star Wars: Episode V - The Empire Strikes Back')
+        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
+           .media([
+            { url: 'http://www.wavlist.com/movies/004/father.wav' }
+        ])
+    
+            // attach the card to the reply message
+        var msg = new builder.Message(session).addAttachment(card);
+        bot.send(msg);
+    
         builder.Prompts.text(session,'what is your name');  
     },
     function(session,results){
@@ -198,21 +211,7 @@ bot.dialog("identifyRole",[
 ]);
 
 bot.on("event", function (event) {
-    var card = builder.AudioCard(event.session)
-        .title('I am your father')
-        .subtitle('Star Wars: Episode V - The Empire Strikes Back')
-        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
-        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'))
-        .media([
-            { url: 'http://www.wavlist.com/movies/004/father.wav' }
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
-        ]);
     
-            // attach the card to the reply message
-    var msg = new builder.Message(event.session).addAttachment(card);
-    bot.send(msg);
     /*var msg = new builder.Message().address(event.address);
     event.ses
     msg.data.textLocale = "en-us";
